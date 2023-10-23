d3bb9f7034be4f6cbb68a6c64d4965a04ebb9ec5
Dipa Galatian <dipagalatian3003@gmail.com>
2023-10-23 11:47:47 +0700

    making setup for Block.vue

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="block">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  margin: 40px auto;
+  padding: 100px 0;
+  text-align: center;
+}
+</style>
